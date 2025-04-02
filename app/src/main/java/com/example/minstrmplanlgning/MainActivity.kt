abb4c92d44632d7b212b5f5ee4f9f2cd796bcbc7
user4230 <nicholasgladik@hotmail.com>
2025-04-02 10:40:02 +0200

    test navigation between 2 screens

--- app/src/main/java/com/example/minstrmplanlgning/MainActivity.kt
+++ app/src/main/java/com/example/minstrmplanlgning/MainActivity.kt
@@ -4,44 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.minstrmplanlgning.ui.theme.MinStrømPlanlægningTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.minstrmplanlgning.ui.navigation.NavigateHomeAndScreen2
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MinStrømPlanlægningTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Column {
+                Spacer(
+                    modifier = Modifier
+                        .size(30.dp)
+                )
+                Text(
+                    text = "Example of navigation between 2 screens",
+                    fontSize = 20.sp
+                )
+                NavigateHomeAndScreen2()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MinStrømPlanlægningTheme {
-        Greeting("Android")
     }
 }
