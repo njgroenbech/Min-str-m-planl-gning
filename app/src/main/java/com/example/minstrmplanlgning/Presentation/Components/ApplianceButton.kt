7a3e3d00751dcc7c87d123f79762f25ad2e0b218
user4230 <nicholasgladik@hotmail.com>
2025-04-27 21:55:05 +0200

    Removed hardcoded appliance

--- app/src/main/java/com/example/minstrmplanlgning/Presentation/Components/ApplianceButton.kt
+++ app/src/main/java/com/example/minstrmplanlgning/Presentation/Components/ApplianceButton.kt
@@ -19,6 +19,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+
+// inherits name, iconres and onclick from addDevice
 @Composable
 fun ApplianceButton(name: String, iconRes: Int, onClick: () -> Unit) {
     Card(
