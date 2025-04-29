6fbd41da9da79864c6d226da75fb99d4375d19c6
user4230 <nicholasgladik@hotmail.com>
2025-04-29 20:30:07 +0200

    Attempt at connecting to API 2

--- app/src/main/java/com/example/minstrmplanlgning/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/minstrmplanlgning/data/remote/RetrofitClient.kt
@@ -6,8 +6,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
     private const val BASE_URL = "https://api.minstroem.app/thirdParty"
 
-    private const val apiKey = API_KEY
-
     val apiService: MinStroemApiService = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
